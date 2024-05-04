472c60d56093bb9422349182c2af1a8a1f526ec3
Sagar Bhatia <sagarbhatia722@gmail.com>
2024-05-04 20:50:50 +0530

    .

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="bg-black">
+      <h1 className="text-2xl">hello</h1>
+    </div>
+  );
+};
+
+export default Home;
